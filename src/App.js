4f8c63177c864d3808ac5ac95096abe04476b2f8
kimkablitz <kim.tran549@gmail.com>
2019-01-08 12:37:06 -0800

    some bugs but functional

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
     this.state = {
       friends: friendsData,
       score: 0,
+      topScore: 0,
+      msg:  "",
       game: []
 
     }
@@ -23,18 +25,29 @@ class App extends Component {
   handleClick(cardId) {
     // console.log(cardId)
     const currentList = this.state.game
-    currentList.push(cardId)
-    console.log(currentList)
-   if(currentList.indexOf(cardId) === -1){
-     console.log("uhoh")
-   }
+      if (!currentList.includes(cardId)){
+           currentList.push(cardId)
+            console.log(currentList)
+            this.setState(prevState => {
 
-    this.setState(prevState => {
-
-      return {
-        score: prevState.score + 1
+              return {
+                score: prevState.score + 1,
+                msg:  "GAME IS IN PROGRESS"
+              }
+            })
       }
-    })
+      else {
+        this.setState(prevState => {
+
+          return {
+            topScore: prevState.score,
+            score: this.state.score - this.state.score,
+            msg:"YOU HAVE GUESSED INCORRECTLY"
+          }
+        })
+        // console.log("uhoh same")
+      }
+
     this.shuffleCards(this.state.friends)
   }
 
@@ -53,7 +66,7 @@ class App extends Component {
 
     return (
       <Wrapper>
-        <Navbar score={this.state.score} />
+        <Navbar score={this.state.score} topScore={this.state.topScore} msg={this.state.msg}/>
         <Jumbotron />
         {this.state.friends.map(friend => <FriendCard friend={friend} key={friend.id} cardId={friend.id} handleClick={this.handleClick} />)
         }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,18 @@
 import React from "react";
 import "./style.css";
-// import Greeting from "../component/Greeting"
 
 
 class Navbar extends React.Component {
-  
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     // msg: "Click here to start",
-  //     isInProgress: true
-  //   }
-  // }
+ 
   render() {
-    // console.log(this.props.score)
-    // let msg
-    // if (this.state.isInProgress){
-    //   msg = "CLICK HERE TO START"
-    // } else{
-      let msg = "GAME IS IN PROGRESS"
-    // }
+  
 
     return (
-      <nav className="navbar navbar-expand-lg  bg-warning">
+      <nav className="navbar navbar-expand-lg  bg-warning sticky-top">
         <div className="brand">
           <a href="/">Tinderee</a>
         </div>
-        <div className="text align-center mx-auto ins" style={{ color: "purple" }}>{msg}</div>
+        <div className="text align-center mx-auto ins" style={{ color: "purple" }}>{this.props.msg}</div>
         <div className="score" pullright="true">
           Score: {this.props.score} | Top Score: {this.props.topScore}
         </div>
