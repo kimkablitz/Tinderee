60932f1e0e214d8c40b8dfe5115dd957714c30f7
kimkablitz <kim.tran549@gmail.com>
2019-01-08 00:47:31 -0800

    ids are saved in an array

--- src/App.js
+++ src/App.js
@@ -14,48 +14,49 @@ class App extends Component {
       friends: friendsData,
       score: 0,
       game: []
-      
+
     }
     this.handleClick = this.handleClick.bind(this)
     this.shuffleCards = this.shuffleCards.bind(this)
   }
-  
+
   handleClick(cardId) {
-    console.log(cardId)
-    //id  of the image was clicked
+    // console.log(cardId)
+    const currentList = this.state.game
+    currentList.push(cardId)
+    console.log(currentList)
+   if(currentList.indexOf(cardId) === -1){
+     console.log("uhoh")
+   }
+
     this.setState(prevState => {
-      return{
-        
-        score: prevState.score +1 
-      } 
+
+      return {
+        score: prevState.score + 1
+      }
     })
     this.shuffleCards(this.state.friends)
   }
 
-  shuffleCards(myArray){
+  shuffleCards(myArray) {
     let arrLength = myArray.length - 1;
-    for (let i = 0; i <= arrLength; i++){
+    for (let i = 0; i <= arrLength; i++) {
       const j = Math.floor(Math.random() * (arrLength));
       let temp = myArray[i];
       myArray[i] = myArray[j]
       myArray[j] = temp
     }
-      this.setState({friends:myArray})
+    this.setState({ friends: myArray })
   }
-  //
-
 
   render() {
 
     return (
       <Wrapper>
-        <Navbar score = {this.state.score}/>
+        <Navbar score={this.state.score} />
         <Jumbotron />
-        {this.state.friends.map(friend => <FriendCard friend={friend} key={friend.id} cardId = {friend.id} handleClick={this.handleClick}/>)
-      }
-        {/* <main onClick={this.handleClick}>
-          {myImages}
-        </main> */}
+        {this.state.friends.map(friend => <FriendCard friend={friend} key={friend.id} cardId={friend.id} handleClick={this.handleClick} />)
+        }
         <Footer />
       </Wrapper>
     );
